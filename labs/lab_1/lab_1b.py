38c6138473fbb17e53154949897771a420ccf5f1
Brighten Sun <developerbws2023@gmail.com>
2026-02-15 15:54:27 -0500

    prevent crash from bad user input

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -9,6 +9,21 @@ and prints the result to the terminal window.
 
 """
 
+def sanitize_input(prompt: str) -> float:
+    """function that takes in a user input and returns a valid number
+    Args:
+        str: A prompt for asking the user
+
+    Returns: 
+        float: the sanitized numeric input by the user
+    """
+    while True:
+        try: 
+            number = float(input(prompt))
+            return number
+        except:
+            print(f"Invalid Input. Please enter in a valid number")
+
 def simple_calculator(operation: str, num1: float, num2: float) -> float:
     """
     Function that takes in two numbers and an operation (add, subtract, multiply, divide),
@@ -22,7 +37,6 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
     Returns:
         float: The result of the operation.
     """
-
     if operation == "add":
         return num1 + num2
     elif operation == "subtract":
@@ -42,8 +56,8 @@ def main():
     print(f"===== Simple Calculator =====")
 
     # Ask the user for sample input    
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+    num1 = sanitize_input("Enter the first number: ")
+    num2 = sanitize_input("Enter the second number: ")
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
     # Perform the calculation and display the result
